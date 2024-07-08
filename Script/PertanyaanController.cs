9c98996d7013a643f771cd6bb1c539b8bfd5c253
ramzi syuhada <ramzi.syuhada@mhs.itenas.ac.id>
2024-07-09 02:31:09 +0700

    Interaksi Pertanyaan

---
+++ Script/PertanyaanController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PertanyaanController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _Canvas;
+    
+    void Start()
+    {
+        
+    }
+    
+    private void _InteractionHit()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
