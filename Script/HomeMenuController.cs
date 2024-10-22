1cc99899f0ee7bc62fc62b53a18eb0d72e57d5ac
ramzi syuhada <ramzi.syuhada@mhs.itenas.ac.id>
2024-10-23 01:28:58 +0700

    Fix Bug Rotate

--- Script/HomeMenuController.cs
+++ Script/HomeMenuController.cs
@@ -8,12 +8,13 @@ public class HomeMenuController : MonoBehaviour
 
     [SerializeField] Transform Player;
     [SerializeField] Transform House;
+    [SerializeField] GameObject _canvas;
     public void PlayGame()
     {
         House.gameObject.SetActive(true);
         Player.GetComponent<TeleportationProvider>().enabled = true;
         Player.GetComponent<ContinuousMoveProviderBase>().enabled = true;
-
+        _canvas.SetActive(false);
     }
 
     public void Quit()
